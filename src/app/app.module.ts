ae0c93be5d2a8a12b275436ded9ea394956f4f91
rajckamariya <r.c.kamariya@gmail.com>
2021-08-20 16:23:27 +0530

    Directive Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight-directive.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, BasicHighlightDirective],
+  declarations: [AppComponent, BasicHighlightDirective, UnlessDirective],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
